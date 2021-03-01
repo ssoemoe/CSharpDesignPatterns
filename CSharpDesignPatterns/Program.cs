b1cfb485fcf99093c83a9945bb8ac552edfa8ff0
Shane Moe <ssoemoe@terpmail.umd.edu>
2021-03-01 00:34:43 -0500

    chore: project init:tada:

---
+++ CSharpDesignPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
